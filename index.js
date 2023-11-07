5653b8b59bc9b49a90a65baefb474a684b0bdd2c
rizkydwiananto <rizkydwiananto@gmail.com>
2023-11-07 22:16:23 +0700

    Penambahan CRUD lowongan favorite

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 
 import ProfilPekerjaRoute from "./routes/ProfilPekerjaRoute.js";
 import ApplyLowonganRoute from "./routes/ApplyLowonganRoute.js";
+import LowonganFavoriteRoute from "./routes/LowonganFavoriteRoute.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(ProfilPekerjaRoute);
 app.use(ApplyLowonganRoute);
+app.use(LowonganFavoriteRoute);
 
 app.listen(process.env.APP_PORT, () => {
   console.log("Server berjalan dengan baik ...");
